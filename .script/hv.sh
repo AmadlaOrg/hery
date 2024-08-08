bfd7a32bf234b4ff8800f356315d7e53277788fb
jnbdz <237008+jnbdz@users.noreply.github.com>
2024-08-08 18:43:32 -0400

    Made small corrections

---
+++ .script/hv.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+hv() {
+  local schema="${1}"
+  local heryfile="${2}"
+  local tmpfile="${heryfile%.hery}.yaml"
+
+  cp "${heryfile}" "$tmpfile"
+  jv "${schema}" "${tmpfile}"
+  rm "${tmpfile}"
+}
+hv "${@}"
