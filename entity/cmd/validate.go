76c96e04308126e2d5fd4429f82820fc92848ff6
jnbdz <237008+jnbdz@users.noreply.github.com>
2024-08-12 20:33:02 -0400

    Added more logic to handle validation

--- entity/cmd/validate.go
+++ entity/cmd/validate.go
@@ -2,23 +2,67 @@ package cmd
 
 import (
 	"github.com/AmadlaOrg/hery/entity"
+	"github.com/AmadlaOrg/hery/entity/get"
 	entityValidation "github.com/AmadlaOrg/hery/entity/validation"
 	"github.com/AmadlaOrg/hery/storage"
 	"github.com/spf13/cobra"
 	"log"
 )
 
+var (
+	isValidateAll bool
+	isRm          bool
+)
+
 var ValidateCmd = &cobra.Command{
 	Use:   "valid",
 	Short: "Validate entity or schemas",
 	Run: func(cmd *cobra.Command, args []string) {
+		if !isValidateAll && (len(args) == 0 || (len(args) == 1 && isRm)) {
+			// Print the usage information (helper message)
+			err := cmd.Help()
+			if err != nil {
+				log.Fatal(err)
+			}
+			return
+		}
+
 		concoct(cmd, args, func(collectionName string, paths *storage.AbsPaths, args []string) {
+			if !isValidateAll {
+				var argsLen = len(args)
+				if isRm {
+					argsLen = argsLen - 1
+				}
+
+				if argsLen > 60 {
+					log.Fatal("too many entity URIs (the limit is 60)")
+				}
+
+				getService := get.NewGetService()
+				err := getService.Get(collectionName, paths, args)
+				if err != nil {
+					log.Fatalf("Error getting entity: %s", err)
+				}
+			}
+
+			println(args)
+			println(isValidateAll)
+
+			for _, arg := range args {
+				println(arg)
+			}
+
 			entityList, err := entity.CrawlDirectoriesParallel(paths.Entities)
 			if err != nil {
 				log.Fatal(err)
 			}
 
-			println(paths.Entities)
+			if len(entityList) == 0 {
+				println("No entity")
+			}
+
+			//println(entityList)
+			//println(paths.Entities)
 
 			entityValidation := entityValidation.NewEntityValidationService()
 
@@ -45,4 +89,18 @@ var ValidateCmd = &cobra.Command{
 			// }
 		})
 	},
+}
+
+func init() {
+	ValidateCmd.PersistentFlags().BoolVarP(
+		&isValidateAll,
+		"all",
+		"a",
+		false,
+		"Validate all entities")
+	ValidateCmd.PersistentFlags().BoolVar(
+		&isRm,
+		"rm",
+		false,
+		"Remove entity after validating if it wasn't already downloaded")
 }
