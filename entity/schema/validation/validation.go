6cd345705054b7ba96245abb62f65459e737c9e0
jnbdz <237008+jnbdz@users.noreply.github.com>
2024-09-14 10:00:48 -0400

    Added some doc in the code to make more clear and detailed the errors.

--- entity/schema/validation/validation.go
+++ entity/schema/validation/validation.go
@@ -18,23 +18,29 @@ type SValidation struct {
 }
 
 // Id validation of JSON-Schema for an entity
+// Multiple layers of validation helps the developer debug with more specific errors
 func (s *SValidation) Id(id, collectionName, entityUri string) error {
+
+	// 1. Validates that the `id` is not empty
 	if id == "" {
-		return fmt.Errorf("schema validation failed: no `id` found in schema")
+		return fmt.Errorf("schema validation failed: `id` is empty or not set")
 	}
 
+	// 2. Validates that the format is good
 	idUrnRegex := regexp.MustCompile(schemaPkg.EntityJsonSchemaIdURN)
 	if !idUrnRegex.MatchString(id) {
-		return fmt.Errorf("schema validation failed: invalid `id` found in schema")
+		return fmt.Errorf("schema validation failed: invalid `id` format")
 	}
 
+	// 3. Validates that the prefix of the HERY URN is standard
 	prefix := s.Schema.GenerateURNPrefix(collectionName)
 	if strings.HasPrefix(id, prefix) {
-		return fmt.Errorf("schema validation failed: invalid `urn` found in schema")
+		return fmt.Errorf("schema validation failed: invalid `urn` prefix (`urn:hery:<collection name>:`)")
 	}
 
+	// 4. Validates that the entire URN is valid
 	if s.Schema.GenerateURN(prefix, entityUri) != id {
-		return fmt.Errorf("schema validation failed: invalid `urn` found in schema")
+		return fmt.Errorf("schema validation failed: invalid `urn` since the other values are not the same as the `_entity` URI")
 	}
 
 	return nil
